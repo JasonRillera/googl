c7c63569b0dd74e4ffd8bd9894c9dc0e0924084c
JasonRillera <jarillera@gmail.com>
2022-02-08 12:32:21 -0800

    update

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,14 +6,14 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('Elon Musk');
+    const [searchTerm, setSearchTerm] = useState('Pikachu');
 
     const getResults = async (type) => {
         setIsLoading(true);
 
         const response = await fetch(`${baseUrl}${type}`, {
             method: 'GET',
-            headers:{
+            headers: {
                 'x-user-agent': 'desktop',
                 'x-rapidapi-host': 'google-search3.p.rapidapi.com',
                 'x-rapidapi-key': process.env.REACT_APP_API_KEY
@@ -22,9 +22,9 @@ export const ResultContextProvider = ({ children }) => {
 
         const data = await response.json();
 
-        if(type.includes('/news')) {
+        if (type.includes('/news')) {
             setResults(data.entries);
-        } else if(type.includes('/images')) {
+        } else if (type.includes('/images')) {
             setResults(data.image_results);
         } else {
             setResults(data.results);
@@ -34,7 +34,7 @@ export const ResultContextProvider = ({ children }) => {
     }
 
     return (
-        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading}}>
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
             {children}
         </ResultContext.Provider>
     )
